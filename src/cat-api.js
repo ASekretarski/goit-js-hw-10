130e7bb10862aa3f6cc1f3c12a346e489331f4bb
ASekretarski <artur.sekretarski@gmail.com>
2023-06-02 14:41:50 +0200

    hw 10 completed

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,24 +1,23 @@
-const selectList = document.querySelector('.breed-select')
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-export default function fetchBreeds() {
-    fetch("https://api.thecatapi.com/v1/breeds?api_key=live_rqK3oyd38AWXZJb7xjlK2r9gU43INARb6XZ8w0bUxgPsfIqDs3eCDfsHe3HiMbTT")
+export function fetchBreeds() {
+    return fetch('https://api.thecatapi.com/v1/breeds?api_key=live_rqK3oyd38AWXZJb7xjlK2r9gU43INARb6XZ8w0bUxgPsfIqDs3eCDfsHe3HiMbTT')
         .then(response => {
-            return response.json();
-        })
-        .then((data) => {
-            const breedList = data;
-            const breedListOptions = [];
-            for (breed of breedList) {
-                const breedListItem = document.createElement('option');
-                breedListItem.value = breed.id;
-                breedListItem.textContent = breed.name;
-                breedListOptions.push(breedListItem)
-                
-            }
-            selectList.append(...breedListOptions)
-        })
-        .catch(err => {
-            console.log(err)
-        })
+            if (!response.ok) {
+        Notify.failure(`Error status ${response.status}`)
+    }
+    return response.json();
+  });
 }
 
+export function fetchCatByBreed(breedId) {
+  return fetch(
+      `https://api.thecatapi.com/v1/images/search??api_key=live_rqK3oyd38AWXZJb7xjlK2r9gU43INARb6XZ8w0bUxgPsfIqDs3eCDfsHe3HiMbTT&breed_ids=${breedId}`)
+      .then(response => {
+    if (!response.ok) {
+        Notify.failure(`Error status ${response.status}`);
+        errorDisplay()
+    }
+    return response.json();
+  });
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,103 @@
-import fetchBreeds from "./cat-api"
-
-const selectList = document.querySelector('.breed-select')
-let breedList;
-
-window.addEventListener("load", fetchBreeds)
+import { fetchBreeds, fetchCatByBreed, fetchByUniqueId } from './cat-api';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SlimSelect from 'slim-select'
 
 
+const selectList = document.querySelector('.breed-select');
+const loader = document.querySelector('.loader');
+const errorMessage = document.querySelector('.error');
+const catInfo = document.querySelector('.cat-info');
+
+selectList.classList.add('invisible');
+catInfo.classList.add('invisible');
+errorMessage.classList.add('invisible')
+
+fetchBreeds()
+  .then(data => createBreedsList(data))
+  .catch(error => {
+    errorDisplay();
+  });
+
+function createDescription(event) {
+    fetchCatByBreed(event.target.value)
+    .then()
+}
+
+
+selectList.addEventListener('change', event => {
+    loader.classList.remove('invisible');
+    catInfo.classList.add('invisible');
+    selectList.classList.add('invisible');
+    fetchCatByBreed(event.target.value)
+        .then(data => {getCatImage(data).catch(error => {
+            errorDisplay();
+        })
+        .finally(() => {
+            catInfo.classList.remove('invisible');
+            selectList.classList.remove('invisible');
+            loader.classList.add('invisible')
+        });
+    });
+    
+});
+
+function createBreedsList(breeds) {
+    const breedsList = [];
+    for (breed of breeds) {
+        const breedListItem = document.createElement('option');
+        breedListItem.value = breed.id;
+        breedListItem.textContent = breed.name;
+        breedsList.push(breedListItem)
+    }
+    selectList.append(...breedsList)
+    selectList.classList.remove('invisible')
+    loader.classList.add('invisible')
+}
+
+function generateDescription(breedName, breedDescription, breedTemperament) {
+    const breedDescriptionContent = [];
+    const breedNameDisplay = document.createElement('h2');
+    breedNameDisplay.textContent = breedName;
+    const breedDescriptionDisplay = document.createElement('p');
+    breedDescriptionDisplay.textContent = breedDescription;
+    const breedTemperamentDisplay = document.createElement('p');
+    breedTemperamentDisplay.textContent = breedTemperament;
+    breedDescriptionContent.push(breedNameDisplay, breedDescriptionDisplay, breedTemperamentDisplay)
+    catInfo.append(...breedDescriptionContent);
+    
+}
+
+function getCatImage(data) {
+  const catImage = data
+    .map(data => {
+      return `<img class="cat-image" src="${data.url}" width="500px">`;
+    })
+    .join('');
+  catInfo.innerHTML = catImage;
+
+  const descriptionCatId = data.map(data => {
+    return data.id;
+  });
+
+  return fetch(`https://api.thecatapi.com/v1/images/${descriptionCatId}`)
+    .then(response => {
+        if (!response.ok) {
+        Notify.failure(`Error status ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+        const breedName = data.breeds[0].name;
+        const breedDescription = data.breeds[0].description;
+        const breedTemperament = data.breeds[0].temperament;
+        generateDescription(breedName, breedDescription, breedTemperament)
+      return data;
+    });
+    
+}
+
+function errorDisplay() {
+  catInfo.classList.add('invisible');
+  loader.textContent = '';
+  selectList.classList.add('invisible');
+}
