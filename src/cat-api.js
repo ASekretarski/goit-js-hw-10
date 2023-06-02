f78669f9b1c35c29f1876bf8c29df70a79668928
ASekretarski <artur.sekretarski@gmail.com>
2023-06-02 15:12:18 +0200

    gh pages test

--- src/cat-api.js
+++ src/cat-api.js
@@ -3,6 +3,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 export function fetchBreeds() {
     return fetch('https://api.thecatapi.com/v1/breeds?api_key=live_rqK3oyd38AWXZJb7xjlK2r9gU43INARb6XZ8w0bUxgPsfIqDs3eCDfsHe3HiMbTT')
         .then(response => {
+            console.log(`this is the response ${response}`)
             if (!response.ok) {
         Notify.failure(`Error status ${response.status}`)
     }
